ea18f6dd3d295ef5f8dacee8b0575630557a3813
tranceyos2419 <tranceyos2419@gmail.com>
2019-09-18 20:48:25 +0900

    from snapshot was added

--- lib/model/character.dart
+++ lib/model/character.dart
@@ -8,13 +8,16 @@ class Character {
 
   Character(this.name, this.img, this.rating, this.reference);
 
-  Character.fromMap(Map<String, dynamic> snapshot, {this.reference})
-      : assert(snapshot['name'] != null),
-        assert(snapshot['img'] != null),
-        assert(snapshot['rating'] != null),
-        name = snapshot['name'],
-        img = snapshot['img'],
-        rating = snapshot['rating'];
+  Character.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['img'] != null),
+        assert(map['rating'] != null),
+        name = map['name'],
+        img = map['img'],
+        rating = map['rating'];
+
+  Character.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
 
   toJson() {
     return {"name": name, "img": img, "rating": rating};
