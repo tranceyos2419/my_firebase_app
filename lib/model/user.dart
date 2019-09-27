08a3bb5f91e0492ea0e27e09b709ba1542ae78d0
tranceyos2419 <tranceyos2419@gmail.com>
2019-09-27 21:25:02 +0900

    created user provider

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,9 +1,9 @@
 class User {
   final String uid;
   final String email;
-  final String imgUrl;
+  final String photoUrl;
   final String displayName;
   final DateTime lastSeen;
 
-  User(this.uid, this.email, this.imgUrl, this.displayName, this.lastSeen);
+  User({this.uid, this.email, this.photoUrl, this.displayName, this.lastSeen});
 }
---
+++ lib/provider/auth.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:my_firebase_app/model/user.dart';
+
+class Auth with ChangeNotifier {
+  var _auth = false;
+  User _user;
+
+  //* Initialization
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = new GoogleSignIn();
+
+  bool get isAuth {
+    return _auth;
+  }
+
+  User get user {
+    if (_user != null) {
+      return _user;
+    } else {
+      throw Error();
+    }
+  }
+
+  Future<FirebaseUser> googleSignIn() async {
+    // Step 1
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+
+    // Step 2
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    //* Making credential
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+
+    //* sign in user with credential and update firebase credential
+    FirebaseUser _firebaseUser = await _firebaseAuth
+        .signInWithCredential(credential)
+        .then((result) => result.user);
+
+    _user = new User(
+        uid: _firebaseUser.uid,
+        email: _firebaseUser.email,
+        photoUrl: _firebaseUser.photoUrl,
+        displayName: _firebaseUser.displayName,
+        lastSeen: new DateTime.now());
+
+    _auth = true;
+    notifyListeners();
+    return _firebaseUser;
+  }
+
+  void signOut() {
+    _firebaseAuth.signOut();
+    _user = null;
+    _auth = false;
+    notifyListeners();
+  }
+}
