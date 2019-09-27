365e619b59cf9e6ec3e823ee45975070a512b3ca
tranceyos2419 <tranceyos2419@gmail.com>
2019-09-27 20:51:30 +0900

    user model was created

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,3 +1,9 @@
 class User {
-  // final String uid;
+  final String uid;
+  final String email;
+  final String imgUrl;
+  final String displayName;
+  final DateTime lastSeen;
+
+  User(this.uid, this.email, this.imgUrl, this.displayName, this.lastSeen);
 }
